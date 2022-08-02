3b50d3f220950958edb85f7903df4c3d16994d29
liyongbin <liyongbin@58.com>
2022-08-02 14:22:45 +0800

    修改&优化代码，使其正常运行

--- 20/web_normal.py
+++ 20/web_normal.py
@@ -8,30 +8,30 @@ import time
 
 
 def crawler():
-	url = "https://movie.douban.com/cinema/later/beijing/"
-	init_page = requests.get(url).content
-	init_soup = BeautifulSoup(init_page, "lxml")
-	
-	all_movies = init_soup.find("div", id = "showing-soon")
-	for each_movie in all_movies.find_all("div", class_ = "item"):
-		all_a_tag = each_movie.find_all("a")
-		all_li_tag = each_movie.find_all("li")
-		
-		movie_name = all_a_tag[1].text
-		url_to_fetch = all_a_tag[1]['href']
-		movie_date = all_li_tag[0].text
-		
-		response_item = requests.get(url_to_fetch).content
-		soup_item = BeautifulSoup(response_item, "lxml")
-		img_tag = soup_item.find("img")
-		
-		print("{} {} {}".format(movie_name, movie_date, img_tag))
-		
-		
-if __name__ == "__main__":
-	start = time.perf_counter()		
-	crawler()
-	end = time.perf_counter()
-	print("常规爬虫运行耗时{}秒".format(end-start))
+    url = "https://movie.douban.com/cinema/later/beijing/"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'}
+    init_page = requests.get(url, headers=headers).content
+    init_soup = BeautifulSoup(init_page, "html.parser")
 
-		
+    all_movies = init_soup.find("div", id="showing-soon")
+    for each_movie in all_movies.find_all("div", class_="item"):
+        all_a_tag = each_movie.find_all("a")
+        all_li_tag = each_movie.find_all("li")
+
+        movie_name = all_a_tag[1].text
+        url_to_fetch = all_a_tag[1]['href']
+        movie_date = all_li_tag[0].text
+
+        response_item = requests.get(url_to_fetch, headers=headers).content
+        soup_item = BeautifulSoup(response_item, "html.parser")
+        img_tag = soup_item.find("img")
+
+        print("电影：{}\n上映： {}\n海报： {}\n\n".format(movie_name, movie_date, img_tag['src']))
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    crawler()
+    end = time.perf_counter()
+    print("常规爬虫运行耗时{}秒".format(end - start))
