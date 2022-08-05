28b5bfb9d0f3152968b9ac720de84162c8eb1998
liyongbin <liyongbin@58.com>
2022-08-05 16:39:40 +0800

    完善示例代码

--- 33/invest.py
+++ 33/invest.py
@@ -8,47 +8,54 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+########## GEMINI 行情接口 ##########
+## https://api.gemini.com/v1/pubticker/:symbol
+
+
 # 获取Gemini交易所报价数据
 def get_price():
-	gemini_ticker = "https://api.gemini.com/v1/pubticker/{}"
-	symbol = "btcusd"
-	btc_data = requests.get(gemini_ticker.format(symbol)).json()
-	print(json.dumps(btc_data, indent = 4))
-	
+    gemini_ticker = "https://api.gemini.com/v1/pubticker/{}"
+    symbol = "btcusd"
+    btc_data = requests.get(gemini_ticker.format(symbol)).json()
+    print(json.dumps(btc_data, indent=4))
+
 
 # 获取最近一个小时交易数据并绘图
 def get_hour_price():
-	# 要获取的数据时间段
-	periods = "3600"
-	
-	# 抓取数据
-	resp = requests.get("https://api.cryptowat.ch/markets/gemini/btcusd/ohlc", params = {
-	"periods" : periods
-	})
-	data = resp.json()
-	
-	# 转换成pandas的data frame
-	df = pd.DataFrame(
-	    data["result"][periods],
-	    columns = [
-	    "收盘时间",
-	    "开盘时间",
-	    "最高价",
-	    "最低价",
-	    "收盘价",
-	    "成交量",
-	    "NA"
-	    ]
-	)
-	
-	# 输出
-	print(df.head())
-	ax = df["收盘价"].plot(figsize = (14, 7))
-	fig = ax.get_figure()
-	fig.savefig("closeprice.png")
+    # 选择要获取的数据时间段
+    periods = "3600"
+
+    # 通过 Http 抓取 btc 历史价格数据
+    resp = requests.get("https://api.cryptowat.ch/markets/gemini/btcusd/ohlc", params={
+        "periods": periods
+    })
+    data = resp.json()
+
+    # 转换成 pandas data frame
+    df = pd.DataFrame(
+        data["result"][periods],
+        columns=[
+            "收盘时间",
+            "开盘时间",
+            "最高价",
+            "最低价",
+            "收盘价",
+            "成交量",
+            "NA"
+        ]
+    )
+
+    # 输出 DataFrame 的头部几行
+    print(df.head())
+
+    # 绘制 btc 价格曲线
+    ax = df["收盘价"].plot(figsize=(14, 7))
+
+    # 绘制图形
+    fig = ax.get_figure()
+    fig.savefig("closeprice.png")
 
 
 if __name__ == "__main__":
-	get_price()
-	get_hour_price()
-	
+    # get_price()
+    get_hour_price()
