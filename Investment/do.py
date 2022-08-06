df9c367b81955cfb0cc14c0d04367aed067534b5
liyongbin <liyongbin@58.com>
2022-08-06 18:16:22 +0800

    完善示例代码

--- Investment/do.py
+++ Investment/do.py
@@ -10,46 +10,43 @@ import hashlib
 import datetime
 import time
 
-
 if __name__ == "__main__":
-	base_url = "https://api.sandbox.gemini.com"
-	endpoint = "/v1/order/new"
-	url = base_url + endpoint
-	
-	gemini_api_key = "account-PsnWZPOnOf0FatEh3pWj"
-	gemini_api_secret = "49wC5psfvDhMmtKaNzmmVg42yYYb"
-	
-	t = datetime.datetime.now()
-	payload_nonce = str(int(time.mktime(t.timetuple()) * 1000))
-	
-	payload = {
-		"request" : "/v1/order/new",
-		"nonce" : payload_nonce,
-		"symbol" : "btcusd",
-		"amount" : "5",
-		"price" : "7428.00",
-		"side" : "buy",
-		"type" : "exchange limit",
-		"options" : ["maker-or-cancel"]
-	}
-	
-	encoded_payload = json.dumps(payload).encode()
-	b64 = base64.b64encode(encoded_payload)
-	signature = hmac.new(bytes(gemini_api_secret, "latin-1"), b64, hashlib.sha384).hexdigest()
-	print(signature)
-	
-	requests_header = {
-		"Content-Type" : "text/plain",
-		"Content-Length" : "0",
-		"X-GEMINI-APIKEY" : gemini_api_key,
-		"X-GEMINI-PAYLOAD" : b64, 
-		"X-GEMINI-SIGNATURE" : signature,
-		"Cache-Control" : "no-cache"
-	}
-	
-	response = requests.post(url,
-	                                               data = None,
-	                                               headers = requests_header)
-	new_order = response.json()
-	print(new_order)
-	
+    base_url = "https://api.sandbox.gemini.com"
+    endpoint = "/v1/order/new"
+    url = base_url + endpoint
+
+    gemini_api_key = "account-IZvU5Qvf8IrILPFw33yI"
+    gemini_api_secret = "UjDoXzvkTTfuGV6eThN35GpuqBF"
+
+    t = datetime.datetime.now()
+    payload_nonce = str(int(time.mktime(t.timetuple()) * 1000))
+
+    # 下单操作信息
+    payload = {
+        "request": "/v1/order/new",
+        "nonce": payload_nonce,
+        "symbol": "btcusd",
+        "amount": "2",
+        "price": "23840.00",
+        "side": "buy",
+        "type": "exchange limit",
+        "options": ["maker-or-cancel"]
+    }
+    encoded_payload = json.dumps(payload).encode()
+    b64 = base64.b64encode(encoded_payload)
+
+    signature = hmac.new(bytes(gemini_api_secret, "latin-1"), b64, hashlib.sha384).hexdigest()
+    print(signature)
+
+    requests_header = {
+        "Content-Type": "text/plain",
+        "Content-Length": "0",
+        "X-GEMINI-APIKEY": gemini_api_key,
+        "X-GEMINI-PAYLOAD": b64,
+        "X-GEMINI-SIGNATURE": signature,
+        "Cache-Control": "no-cache"
+    }
+
+    response = requests.post(url, data=None, headers=requests_header)
+    new_order = response.json()
+    print(new_order)
