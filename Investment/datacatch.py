21d74ddb054c5e25ed21d5d1ce80b7577d174804
liyongbin <liyongbin@58.com>
2022-08-08 14:50:12 +0800

    完善示例代码

--- Investment/datacatch.py
+++ Investment/datacatch.py
@@ -4,68 +4,72 @@
 
 import requests
 import timeit
+import time
 import websocket
-import threading
+import _thread as thread
 import ssl
 import json
 
 
 # 获取报价
 def get_orderbook():
-	orderbook = requests.get("https://api.gemini.com/v1/book/btcusd").json()
-	
-	
+    orderbook = requests.get("https://api.gemini.com/v1/book/btcusd").json()
+
+
 # WebSocket测试
 # 在接收到服务器发送的消息时调用
 def on_message(ws, message):
-	print("接受到:" + message)
-	with open("BTCUSD.txt", "a+") as f:
-			print(message)
-			f.write(json.dumps(message) + "/n")
-	
+    print("接受到:" + message)
+    # with open("BTCUSD.txt", "a+") as f:
+    #     print(message)
+    #     f.write(json.dumps(message) + "/n")
 
-# 在和服务器建立完全连接时调用
+
+# 在和服务器建立完成连接时调用
 def on_open(ws):
-	# 线程运行函数
-	def gao():
-		# 往服务器发送0-4，每次发送完休息0.1秒
-		for i in range(5):
-			time.sleep(0.1)
-			msg = "{0}".format(i)
-			ws.send(msg)
-			print("发送了:" + msg)
-		# 休息1秒用于接受服务器回复的消息
-		time.sleep(1)
-		
-		# 关闭websocket连接
-		ws.close()
-		print("websocket关闭了")
-	
-	# 在另一个线程运行gao函数
-	threading.start_new_thread(gao, ())
+    # 线程运行函数
+    def gao():
+        # 往服务器发送0-4，每次发送完休息0.1秒
+        for i in range(5):
+            time.sleep(0.1)
+            msg = "{0}".format(i)
+            ws.send(msg)
+            print("发送了:" + msg)
+
+        # 休息1秒用于接受服务器回复的消息
+        time.sleep(1)
+
+        # 关闭websocket连接
+        ws.close()
+        print("websocket关闭了")
+
+    # 在另一个线程运行gao函数
+    thread.start_new_thread(gao, ())
 
 
-# 获取报价
+# 全局计数器
 count = 5
 
+
 def on_mes(ws, message):
-	global count
-	print(message)
-	count -= 1
-	print("count={}".format(count))
-	# 接受五次消息后关闭
-	if count == 0:
-		ws.close()
+    global count
+    print(message)
+    count -= 1
+    print("count={}".format(count))
+    # 接收了 5 次消息之后关闭 websocket 连接
+    if count == 0:
+        ws.close()
 
 
 if __name__ == "__main__":
-	'''
-	n = 10
-	latency = timeit.timeit("get_orderbook()", setup = "from __main__ import get_orderbook", number = n)
-	print("Latency is {} ms.".format(latency * 1000))
-	'''
-	# ws = websocket.WebSocketApp("ws://echo.websocket.org", on_message = on_message, on_open = on_open)
-	# ws.run_forever()
-	ws = websocket.WebSocketApp("wss://api.gemini.com/v1/marketdata/btcusd?top_of_book=true&offers=true", on_message = on_message)
-	ws.run_forever(sslopt={"cert_reqs" : ssl.CERT_NONE})
-	
+    # n = 10
+    # # latency = timeit.timeit("get_orderbook()", setup="from __main__ import get_orderbook", number=n) * 1.0 / n
+    # latency = timeit.timeit(stmt=get_orderbook, number=n) * 1.0 / n
+    # print("Latency is {} ms.".format(latency * 1000))
+
+    # ws = websocket.WebSocketApp("wss://echo.websocket.events/", on_message=on_message, on_open=on_open)
+    # ws.run_forever()
+
+    ws = websocket.WebSocketApp("wss://api.gemini.com/v1/marketdata/btcusd?top_of_book=true&offers=true",
+                                on_message=on_mes)
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
