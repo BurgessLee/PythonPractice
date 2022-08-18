e5b5b8dc3428aa29f675ec03f3381e2130e0c824
liyongbin <liyongbin@58.com>
2022-08-18 19:53:19 +0800

    完善示例代码

--- Investment/info_post.py
+++ Investment/info_post.py
@@ -7,19 +7,18 @@ import zmq
 
 
 def run():
-	context = zmq.Context()
-	socket = context.socket(zmq.PUB)
-	socket.bind("tcp://*:6666")
-	socket.setsockopt_string(zmq.SUBSCRIBE, '')
-	
-	cnt = 1
-	
-	while True:
-		time.sleep(1)
-		socket.send_string("server cnt {}".format(cnt))
-		print("send {}".format(cnt))
-		cnt += 1
-	
-	
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind("tcp://*:6666")
+
+    cnt = 1
+
+    while True:
+        time.sleep(1)
+        socket.send_string("server cnt {}".format(cnt))
+        print("send {}".format(cnt))
+        cnt += 1
+
+
 if __name__ == "__main__":
-	run()
+    run()
